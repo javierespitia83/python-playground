da6d1c3d840463eff21bf6c73152a871b5bf6f0a
Javier Espitia <javierespitia83@gmail.com>
2026-02-18 08:20:39 -0500

    new:variables.py

--- 00_git_test.py
+++ 00_git_test.py
@@ -4,3 +4,16 @@ Author: [javierespitia83]
 Version: 1.0.0
 date: 2026-02-12
 """
+"""
+git init: Crea un nuevo repositorio local en la carpeta actual.
+git status: Muestra el estado actual de tu directorio de trabajo 
+git add <archivo>: Añade un archivo específico al área de preparación (staging area).
+git commit -m "Mensaje descriptivo": Guarda los cambios que están en el área de preparación.
+git remote add origin <url-del-repo>: Conecta tu repositorio local con un repositorio remoto.
+git push -u origin <nombre-rama>: Sube tus cambios locales a GitHub por primera vez
+git push: Sube los commits locales a la rama remota 
+git pull: Descarga los últimos cambios desde GitHub y los fusiona con tu código local.
+git log: Muestra el historial de commits 
+
+"""
+
---
+++ 00_python_variables.py
@@ -0,0 +1,12 @@
+"""Project: Testig variables and methods
+Description: File to test git
+Author: [javierespitia83]
+Version: 1.0.0
+date: 2026-02-118
+"""
+
+texto = "Hola Javier"
+texto =  texto.replace("Javier", "Carlos")
+email = "JAVIERESPITIA83@GMAIL.COM"
+email = email.lower()
+print(email)
